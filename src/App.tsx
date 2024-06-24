03ccd124c7cc509a27dfc16a042c82279b19dcae
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-24 12:00:14 +0600

    reducer hook learned commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { useReducer } from "react";
 import "./App.css";
 import "./index.css";
+
+const initialState = {
+  name: "",
+  age: "",
+  email: "",
+};
+
+const reducer = (
+  currentState: { name: string; age: string; email: string },
+  action: { type: string; payload: string }
+) => {
+  switch (action.type) {
+    case "addName":
+      return { ...currentState, name: action.payload };
+
+    case "addAge":
+      return { ...currentState, age: action.payload };
+
+    case "addEmail":
+      return { ...currentState, email: action.payload };
+
+    default:
+      return currentState;
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
   return (
     <div className="App p-24">
       <form className="p-20 bg-slate-100 flex gap-10">
         <input
+          onChange={(e) =>
+            dispatch({ type: "addName", payload: e.target.value })
+          }
           className="md:text-base text-sm p-2"
           placeholder="Input Name"
           type="text"
@@ -12,6 +44,9 @@ function App() {
           id="name"
         />
         <input
+          onChange={(e) =>
+            dispatch({ type: "addAge", payload: e.target.value })
+          }
           className="md:text-base text-sm p-2"
           placeholder="Input Age"
           type="number"
@@ -19,6 +54,9 @@ function App() {
           id="age"
         />
         <input
+          onChange={(e) =>
+            dispatch({ type: "addEmail", payload: e.target.value })
+          }
           className="md:text-base text-sm p-2"
           placeholder="Input Email"
           type="email"
